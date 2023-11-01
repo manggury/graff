6f00b90f8b5fbeb61ac1bcec6a4bec595990a4c0
manggury <148739312+manggury@users.noreply.github.com>
2023-11-01 15:14:58 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,35 @@
 $(function () {
-    $('.btn').on('click', function () {
-        $('h1').toggle();
+    // 문서가 준비되면 일을 시작해라.
+    $('.main_visual_slide').slick({
+        arrows: false
+    });
+
+
+    $('.Main_product_slide').slick({
+        slidesToShow: 5,
+        arrows: false,
+        dots: true
+    });
+
+    $('.MainProduct .arrows .left').on('click', function () {
+        $('.Main_product_slide').slick('slickPrev');
+    });
+    $('.MainProduct .arrows .right').on('click', function () {
+        $('.Main_product_slide').slick('slickNext');
+    });
+
+
+    $('.totop').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 1000)
+    });
+
+
+    $(window).on('scroll', function () {
+        const sct = $(window).scrollTop();
+        if (sct > 200) {
+            $('.totop').addClass('on')
+        } else {
+            $('.totop').removeClass('on');
+        }
     })
-})
+});
