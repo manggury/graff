00ccbb29fa7e0a4f8ce6374b7fb0bc202687cb9c
manggury <148739312+manggury@users.noreply.github.com>
2023-11-02 11:17:58 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -32,4 +32,36 @@ $(function () {
             $('.totop').removeClass('on');
         }
     })
+
+    $('.tab_menu li').on('click', function (e) {
+        // $(this) : click한 자신
+        // $(this).index() : click한 자신의 고유번호
+
+        e.preventDefault(); //a tag의 이벤트 자체를 멈춘다.
+        const idx = $(this).index(); //0,1,2
+        console.log('내가 클릭한 것의 번호: ', idx)
+
+        // eq(idx) : idx 번쨰 자식을 구해라.
+        // siblings() : 나머지형제를 구해라.
+        $('.tab_content .content')
+            .eq(idx)
+            .addClass('on')
+            .siblings()
+            .removeClass('on');
+
+        $(this)
+            .addClass('on')
+            .siblings()
+            .removeClass('on');
+    })
+
+    $('.lnk').on('change', function () {
+        const lnk = $(this).val();
+        console.log(lnk);
+        // if (lnk) = lnk &&
+
+        lnk && window.open(lnk);
+
+    })
+
 });
